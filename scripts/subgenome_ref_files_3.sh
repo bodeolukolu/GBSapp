89f2e49c62961b2760dba8c9abce3b6883158a33
bodeolukolu <bolukolu@utk.edu>
2022-03-19 21:42:53 -0400

    update to produce hapmap format

--- scripts/subgenome_ref_files_3.sh
+++ scripts/subgenome_ref_files_3.sh
@@ -514,10 +514,79 @@ mainCFI_check () {
 		fi
 	done
 	END=10
-	while [[ $(wc -l ../report_fq_compress_index.txt | awk '{print $1}') -gt 0 ]] && [[ $END -gt 0 ]]; do
+	while [[ $END -gt 0 ]]; do
 		echo -e "${magenta}- $(wc -l ../report_fq_compress_index.txt | awk '{print $1}') fastq file not properly processed ${white}\n"
 		echo -e "${magenta}- re-submitting fastq Compression/Indexing function to process only interrupted fastq file processing ${white}\n"
-	  time mainCFI &>> log.out
+		for i in $( cat ${projdir}/${samples_list} ); do
+			if [[ "$lib_type" == "RRS" ]] && test ! -f ${projdir}/compress_done.txt && test ! -f ${projdir}/organize_files_done.txt && test ! -f ${projdir}/preprocess/${i%.f*}_redun.sam && test ! -f ${projdir}/preprocess/${i%.f*}_${ref1%.f*}_precall.bam.bai; then
+				if test ! -f ${i%.f*}_uniq_R1.fasta; then
+					if [[ $(file $i | awk -F' ' '{print $2}') == gzip ]]; then
+						zcat $i | awk 'NR%2==0' | awk 'NR%2' | gzip > ${i%.f*}_uniq.txt.gz 2> /dev/null &&
+						wait
+						if test -f ${i%.f*}_R2*; then
+							zcat ${i%.f*}_R2* | awk 'NR%2==0' | awk 'NR%2' | gzip > ${i%.f*}_R2_uniq.txt.gz 2> /dev/null &&
+							wait
+						fi
+						wait
+						if test -f ${i%.f*}.R2*; then
+							zcat ${i%.f*}.R2* | awk 'NR%2==0' | awk 'NR%2' | gzip > ${i%.f*}_R2_uniq.txt.gz 2> /dev/null &&
+							wait
+						fi
+						wait
+					else
+						awk 'NR%2==0' $i | awk 'NR%2' | gzip > ${i%.f*}_uniq.txt.gz 2> /dev/null &&
+						wait
+						if test -f ${i%.f*}_R2*; then
+							awk 'NR%2==0' ${i%.f*}_R2* | awk 'NR%2' | gzip > ${i%.f*}_R2_uniq.txt.gz 2> /dev/null &&
+							wait
+						fi
+						wait
+						if test -f ${i%.f*}.R2*; then
+							awk 'NR%2==0' ${i%.f*}.R2* | awk 'NR%2' | gzip > ${i%.f*}_R2_uniq.txt.gz 2> /dev/null &&
+							wait
+						fi
+						wait
+					fi
+					wait
+					if test -f "${i%.f*}_R2_uniq.txt.gz"; then
+						zcat ${i%.f*}_uniq.txt.gz | printf "${i%.f*}""\t""$(wc -l)""\n" > ${projdir}/alignment_summaries/${i%.f*}_total_read_count.txt  2> /dev/null &&
+						wait
+						export LC_ALL=C; paste -d ~ <(zcat ${i%.f*}_uniq.txt.gz) <(zcat ${i%.f*}_R2_uniq.txt.gz 2> /dev/null) | expand -t $(( $(wc -L < $i ) + 2 )) | awk '{!seen[$0]++}END{for (i in seen) print seen[i], i}' | awk '{$1=$1};1' | \
+						awk '{gsub(" /"," "); print}' | awk '{gsub("/\n","\n"); print}' | awk '{gsub("/"," "); print}' | awk '{gsub(" ","\t"); print}' | gzip > ${i%.f*}_rdrefseq.txt.gz 2> /dev/null &&
+						wait
+						awk 'NF==2 {print ">seq"NR"_se-"$1"\t"$2}' <(zcat ${i%.f*}_rdrefseq.txt.gz 2> /dev/null) | gzip > ${i%.f*}_rdrefseq_se.txt.gz 2> /dev/null &&
+						wait
+						awk 'NF==3 {print ">seq"NR"_pe-"$0}' <(zcat ${i%.f*}_rdrefseq.txt.gz 2> /dev/null) | awk '{print $1"\t"$3}' | gzip > ${i%.f*}_uniq_R2.fasta.gz 2> /dev/null &&
+						wait
+						awk 'NF==3 {print ">seq"NR"_pe-"$0}' <(zcat ${i%.f*}_rdrefseq.txt.gz 2> /dev/null) | awk '{print $1"\t"$2}' | gzip | cat - <(zcat ${i%.f*}_rdrefseq_se.txt.gz) > ${i%.f*}_uniq_R1.hold.fasta.gz 2> /dev/null &&
+						wait
+						rm ${i%.f*}*.txt* 2> /dev/null &&
+						wait
+						find . -size 0 -delete  2> /dev/null &&
+						wait
+						mv ${i%.f*}_uniq_R1.hold.fasta.gz ${i%.f*}_uniq_R1.fasta.gz  2> /dev/null &&
+						wait
+					else
+						touch ${i%.f*}_R2_uniq.txt &&
+						wait
+						zcat ${i%.f*}_uniq.txt.gz | printf "${i%.f*}""\t""$(wc -l)""\n" > ${projdir}/alignment_summaries/${i%.f*}_total_read_count.txt  2> /dev/null &&
+						wait
+						export LC_ALL=C; paste -d ~ <(zcat ${i%.f*}_uniq.txt.gz) ${i%.f*}_R2_uniq.txt | expand -t $(( $(wc -L < $i ) + 2 )) | awk '{!seen[$0]++}END{for (i in seen) print seen[i], i}' | awk '{$1=$1};1' | \
+						awk '{gsub(" /"," "); print}' | awk '{gsub("/\n","\n"); print}' | awk '{gsub("/"," "); print}' | awk '{gsub(" ","\t"); print}' | gzip > ${i%.f*}_rdrefseq.txt.gz 2> /dev/null &&
+						wait
+						awk 'NF==2 {print ">seq"NR"_pe-"$0}' <(zcat ${i%.f*}_rdrefseq.txt.gz 2> /dev/null) | awk '{print $1"\t"$2}' | gzip > ${i%.f*}_uniq_R1.hold.fasta.gz 2> /dev/null &&
+						wait
+						rm ${i%.f*}*.txt* 2> /dev/null &&
+						wait
+						find . -size 0 -delete  2> /dev/null &&
+						wait
+						mv ${i%.f*}_uniq_R1.hold.fasta.gz ${i%.f*}_uniq_R1.fasta.gz  2> /dev/null &&
+						wait
+					fi
+					wait
+				fi
+			fi
+		done
 	  END=$(($END-1))
 		: > ../report_fq_compress_index.txt
 		for i in *_uniq_R1.fasta.gz; do
@@ -541,6 +610,7 @@ else
 		time mainCFI_check &>> log.out
 	fi
 fi
+
 
 
 
@@ -2748,7 +2818,7 @@ for snpfilter_dir in $(ls -d */); do
 			Rscript "${GBSapp_dir}"/scripts/R/hapmap_format.R "$i" "${i%.txt}.hmp.txt"
 		done
 		wait
-		
+
 		cd ../../
 	fi
 done
