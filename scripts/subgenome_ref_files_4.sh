9c74e7602afe5c8c0ff3f4243a256ad751c45c2f
bodeolukolu <bolukolu@utk.edu>
2022-03-27 22:07:59 -0400

    optimizing multi-threading and multi-processing

--- scripts/subgenome_ref_files_4.sh
+++ scripts/subgenome_ref_files_4.sh
@@ -618,7 +618,7 @@ main () {
 						awk '{print $2}' ${projdir}/alignment_summaries/background_mutation_test/pop_haps_freqFail.txt | \
 						grep -v -x -f - <(zcat ${projdir}/samples/${i%.f*}_uniq_R2.fasta.gz 2> /dev/null) | awk '{gsub(/>/,"@"); print}' | \
 						awk '{print $1"\t"$2"\t"$2}' | awk 'BEGIN{OFS="\t"}{gsub(/A|a|C|c|G|g|T|t|N|n/,"I",$3); print}' | awk '{print $1"/2\n"$2"\n+\n"$3}' | $gzip  > ${projdir}/samples/${i%.f*}_uniq_R2.fq.gz &&
-						rm ${projdir}/samples/${i%.f*}_uniq_R*.fasta.gz
+						rm "${projdir}/samples/${i%.f*}_uniq_R*.fasta.gz"
 						wait
 					else
 						awk '{print $2}' ${projdir}/alignment_summaries/background_mutation_test/pop_haps_freqFail.txt | \
@@ -633,7 +633,7 @@ main () {
 						zcat ${projdir}/samples/${i%.f*}_uniq_R1.fasta.gz 2> /dev/null | awk -F '\t' '$1~/_pe-/{print}' | awk '{gsub(/>/,"@"); print}' | awk '{print $1"\t"$2"\t"$2}' | awk 'BEGIN{OFS="\t"}{gsub(/A|a|C|c|G|g|T|t|N|n/,"I",$3); print}' | awk '{print $1"/1\n"$2"\n+\n"$3}' | $gzip > ${projdir}/samples/${i%.f*}_uniq_R1.fq.gz &&
 						zcat ${projdir}/samples/${i%.f*}_uniq_R2.fasta.gz 2> /dev/null | awk '{gsub(/>/,"@"); print}' | awk '{print $1"\t"$2"\t"$2}' | \
 						awk 'BEGIN{OFS="\t"}{gsub(/A|a|C|c|G|g|T|t|N|n/,"I",$3); print}' | awk '{print $1"/2\n"$2"\n+\n"$3}' | $gzip > ${projdir}/samples/${i%.f*}_uniq_R2.fq.gz &&
-						rm ${projdir}/samples/${i%.f*}_uniq_R*.fasta.gz
+						rm "${projdir}/samples/${i%.f*}_uniq_R*.fasta.gz"
 						wait
 					else
 						zcat ${projdir}/samples/${i%.f*}_uniq_R1.fasta.gz 2> /dev/null | awk '{gsub(/>/,"@"); print}' | awk '{print $1"\t"$2"\t"$2}' | \
