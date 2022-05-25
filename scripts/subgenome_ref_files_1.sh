ff57887f8401272d4df4295053255bcd8e89121f
bodeolukolu <bolukolu@utk.edu>
2022-05-25 09:24:50 -0400

    update to minimize excess heterozygosity

--- scripts/subgenome_ref_files_1.sh
+++ scripts/subgenome_ref_files_1.sh
@@ -733,7 +733,7 @@ main () {
 		cp ${projdir}/preprocess/combined_all_sample_reads_redun.sam.gz /tmp/${samples_list%.txt}/preprocess/ 2> /dev/null &&
 		if [[ "$lib_type" == "RRS" ]]; then
 			for i in $(cat ${projdir}/${samples_list} ); do
-				mv ${i%.f*}_uniq_R1.fasta.gz /tmp/${samples_list%.txt}/samples/ 2> /dev/null &&
+				cp ${i%.f*}_uniq_R*.fasta.gz /tmp/${samples_list%.txt}/samples/ 2> /dev/null &&
 				cp merged_index.txt.gz /tmp/${samples_list%.txt}/samples/ 2> /dev/null &&
 				cp ${projdir}/preprocess/${i%.f*}_redun.sam.gz /tmp/${samples_list%.txt}/preprocess/ 2> /dev/null &&
 				cp ${projdir}/preprocess/${i%.f*}_*_precall.bam* /tmp/${samples_list%.txt}/preprocess/ 2> /dev/null &&
