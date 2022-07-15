8df7567d8670e16636d6e61dfc732eef3f15ea75
bodeolukolu <bolukolu@utk.edu>
2022-07-15 15:19:39 -0400

    multi-node alignment

--- scripts/subgenome_ref_files_3.sh
+++ scripts/subgenome_ref_files_3.sh
@@ -828,14 +828,8 @@ main () {
 	wait
 
 	cd ${projdir}/preprocess
-	find . -name '*.bam' | {
-	    read firstbam
-	    $samtools view -h "$firstbam"
-	    while read bam; do
-	        $samtools view "$bam"
-	    done
-	} | $samtools view -ubS - | $samtools sort - merged
-	$samtools view -h merged.bam > combined_all_sample_reads_redun.sam
+	$samtools view -H combined_all_sample_reads_R1_chunk1.bam > combined_all_sample_reads_redun.sam
+	find ./ -name \*.bam -exec $samtools view {} \; >> combined_all_sample_reads_redun.sam
 	$gzip combined_all_sample_reads_redun.sam
 
 	wait && touch ${projdir}/alignment_done_${samples_list}
